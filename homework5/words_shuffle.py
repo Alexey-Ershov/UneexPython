c38042a05e5e23d2c444ce7dd2e2f19e792b20ec
Alexey-Ershov <ershov_ae@mail.ru>
2018-11-02 22:18:17 +0300

    Fix while condition + elision marks

--- homework5/words_shuffle.py
+++ homework5/words_shuffle.py
@@ -4,7 +4,8 @@ words_from_first_string = input().split()
 
 i = 0
 was_cut = False
-while i < len(words_from_first_string):
+list_len = len(words_from_first_string)
+while i < list_len:
     if not was_cut:
         words_from_first_string[i] = words_from_first_string[i].lower()
 
@@ -14,11 +15,18 @@ while i < len(words_from_first_string):
     word = words_from_first_string[i]
     for index, char in enumerate(word):
         if char in punct_chars:
+            if char == '.' and word[index + 1:index + 3] == '..':
+                char = '...'
+                tail_index = index + 3
+
+            else:
+                tail_index = index + 1
+
             head = word[:index]
             if head:
                 words_from_first_string.append(head)
 
-            tail = word[index + 1:]
+            tail = word[tail_index:]
             if tail:
                 words_from_first_string[i] = tail
                 words_from_first_string.append(char)
@@ -27,6 +35,8 @@ while i < len(words_from_first_string):
 
             else:
                 words_from_first_string[i] = char
+                if tail_index - index == 3:
+                    break
 
     if was_cut:
         continue
